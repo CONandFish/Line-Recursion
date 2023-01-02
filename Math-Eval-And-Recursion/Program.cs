8152fb9048d6fc9516c58503cc7a25c4f3a532eb
con_f <con_f@CONANDFISH>
2023-01-02 13:02:36 -0600

    Add project files.

---
+++ Math-Eval-And-Recursion/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Math {
+    public static void Main()
+    {
+        recurse(1, 1, 5, 5, new Point(1, 1));
+    }
+
+    public static void recurse(int bx, int by, int ex, int ey, Point slope)
+    {
+        Console.WriteLine($"point: {bx}");
+        Console.WriteLine($"x: {slope.x + bx}");
+        Console.WriteLine($"y: {slope.y + by} \n");
+
+        if(bx >= ex || by >= ey)
+        {
+            return;
+        }
+         else
+        {
+            recurse(bx + slope.x, by + slope.y, ex, ey, slope);
+        }
+    }
+}
+
